04ac016e277473985e50a99ed2ceaf1e197a5435
Nishchal, Praveen <praveen.nishchal@intel.com>
2023-04-26 16:28:37 +0530

    junit test issues fixed

--- src/test/java/com/intel/qat/QATTest.java
+++ src/test/java/com/intel/qat/QATTest.java
@@ -17,8 +17,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 
