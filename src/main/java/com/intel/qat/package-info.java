e10f03c543bf7cbae52a3958a11c87e612664eae
Olasoji <olasoji.denloye@intel.com>
2023-07-31 14:29:58 -0400

    Creates qat Module - Adds module-info.java and description - Adds package-info.java and description - Changed InternalJNI.java to package private - modified surefire version in pom to fix build error

---
+++ src/main/java/com/intel/qat/package-info.java
@@ -0,0 +1,14 @@
+/*******************************************************************************
+ * Copyright (C) 2023 Intel Corporation
+ *
+ * SPDX-License-Identifier: BSD
+ ******************************************************************************/
+
+/**
+ * This package provides classes needed to compress and decompress data using Intel's QuickAssist Technology.
+ *
+ * @since 1.0
+ * @see com.intel.qat
+ */
+
+package com.intel.qat;
---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ * Copyright (C) 2023 Intel Corporation
+ *
+ * SPDX-License-Identifier: BSD
+ ******************************************************************************/
+
+/**
+ * Defines APIs for data compression using Intel QuickAssit Technology.
+ */
+module qat {
+ 	exports com.intel.qat;
+}
+
