582f850cfc974594f8d2d3178bfc404813c8c3d3
Jacob Greenfield <jag625@lehigh.edu>
2023-11-11 22:01:56 +0000

    Switch to new, safer sequence producer API

--- src/main/java/com/intel/qat/QatZstdSequenceProducer.java
+++ src/main/java/com/intel/qat/QatZstdSequenceProducer.java
@@ -1,36 +1,13 @@
 package com.intel.qat;
 
 import com.github.luben.zstd.SequenceProducer;
-import java.lang.ref.Cleaner;
 
 /**
- * An implementation of an abstract sequence producer plugin. To be used in conjunction with
- * zstd-jni, passed as a
+ * An implementation of an abstract sequence producer plugin. Pass an instance of this class to
+ * {@link com.github.luben.zstd.ZstdCompressCtx#registerSequenceProducer(SequenceProducer)} to
+ * enable QAT-accelerated compression with zstd.
  */
-public class QatZstdSequenceProducer implements SequenceProducer, AutoCloseable {
-  /** Pointer to the `sequenceProducerState` */
-  private long sequenceProducerState;
-
-  /** Cleaner instance associated with this object. */
-  private static Cleaner cleaner;
-
-  /** Cleaner.Cleanable instance representing zstd cleanup action. */
-  private final Cleaner.Cleanable cleanable;
-
-  static {
-    SecurityManager sm = System.getSecurityManager();
-    if (sm == null) {
-      cleaner = Cleaner.create();
-    } else {
-      java.security.PrivilegedAction<Void> pa =
-          () -> {
-            cleaner = Cleaner.create();
-            return null;
-          };
-      java.security.AccessController.doPrivileged(pa);
-    }
-  }
-
+public class QatZstdSequenceProducer implements SequenceProducer {
   public static final class Status {
     /** Success. */
     public static final int OK = 0;
@@ -50,43 +27,20 @@ public class QatZstdSequenceProducer implements SequenceProducer, AutoCloseable 
     InternalJNI.zstdStopDevice();
   }
 
-  public QatZstdSequenceProducer() {
-    sequenceProducerState = InternalJNI.zstdCreateSeqProdState();
-    assert sequenceProducerState != 0;
-    cleanable = cleaner.register(this, new ZstdCleaner(sequenceProducerState));
-  }
+  public QatZstdSequenceProducer() {}
 
   @Override
-  public long getProducerFunction() {
+  public long getFunctionPointer() {
     return InternalJNI.zstdGetSeqProdFunction();
   }
 
   @Override
-  public long getStatePointer() {
-    return sequenceProducerState;
+  public long createState() {
+    return InternalJNI.zstdCreateSeqProdState();
   }
 
   @Override
-  public void close() {
-    cleanable.clean();
-  }
-
-  /** A class that represents a cleaner action for a zstd session. */
-  private static class ZstdCleaner implements Runnable {
-    private long sequenceProducerState;
-
-    /** Creates a new cleaner object that cleans up the specified session. */
-    public ZstdCleaner(long sequenceProducerState) {
-      this.sequenceProducerState = sequenceProducerState;
-    }
-
-    @Override
-    public void run() {
-      if (sequenceProducerState == 0)
-        throw new RuntimeException("Attempted to double-free the sequence producer state");
-
-      InternalJNI.zstdFreeSeqProdState(sequenceProducerState);
-      sequenceProducerState = 0;
-    }
+  public void freeState(long state) {
+    InternalJNI.zstdFreeSeqProdState(state);
   }
 }
