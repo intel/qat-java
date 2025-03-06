abfbbb9fe8184ffaff806a5e8781e881eed3cccb
Mulugeta Mammo <mulugeta.mammo@intel.com>
2025-03-06 21:53:37 +0000

    Bug fix for zstd. Improve cmake build.
    Signed-off-by: Mulugeta Mammo <mulugeta.mammo@intel.com>

--- src/main/java/com/intel/qat/QatZipper.java
+++ src/main/java/com/intel/qat/QatZipper.java
@@ -441,9 +441,11 @@ public class QatZipper {
     if (algorithm == Algorithm.ZSTD) {
       final int QZ_OK = 0; // indicates that ZSTD can start QAT device
       if (mode == Mode.HARDWARE || (mode == Mode.AUTO && status == QZ_OK)) {
-        // Only if mode is HARDWARE or AUTO with QAT device started
-        zstdCompressCtx.registerSequenceProducer(new QatZstdSequenceProducer());
+        // Important! Order matters here!
+        // 1. first, set sequence producer fallback
         zstdCompressCtx.setSequenceProducerFallback(true);
+        // 2. then, register the sequence producer.
+        zstdCompressCtx.registerSequenceProducer(new QatZstdSequenceProducer());
       }
       zstdCompressCtx.setLevel(level);
     }
